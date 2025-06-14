2b6cffe487cbf8283f2d28542a291dc810c53856
Triddov <akinin.stepan7@gmail.com>
2025-06-14 17:04:43 +0300

    init commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+
+async function start(){
+
+    const PORT = process.env.PORT || 80;
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(PORT, () => { console.log(`Listening on ${PORT}`); })
+}
+
+start()
+
+
+
+
